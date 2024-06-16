9cf1ed047cd5c25ee47409b843282664aa269db3
Mairo022 <mairo022@outlook.com>
2024-06-16 22:06:01 +0300

    Use angular form

--- src/app/core/components/flights/flights.component.ts
+++ src/app/core/components/flights/flights.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { BookingDialogueComponent } from '../booking-dialogue/booking-dialogue.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-flights',
   standalone: true,
-  imports: [BookingDialogueComponent],
+  imports: [BookingDialogueComponent, ReactiveFormsModule],
   templateUrl: './flights.component.html',
   styleUrl: './flights.component.scss'
 })
 export class FlightsComponent {
   isBookingDialogueOpen: boolean = false;
   isBookingRowOpen: boolean = false
+
+  routeForm: FormGroup
+  
+  constructor(private fb: FormBuilder) {
+    this.routeForm = this.fb.group({
+      from: null,
+      to: null
+    })
+  }
+
+  onSubmit(): void {
+
+  }
 
   setBookingRowOpen() {
     this.isBookingRowOpen = !this.isBookingRowOpen
@@ -21,5 +35,4 @@ export class FlightsComponent {
   closeBookingDialogue() {
     this.isBookingDialogueOpen = false
   }
-
 }
