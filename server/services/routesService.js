3bc76abb372f44b386c2a6045f4912d77bfc3a85
Mairo022 <mairo022@outlook.com>
2024-07-07 14:49:22 +0300

    Return all offers for routes request

--- server/services/routesService.js
+++ server/services/routesService.js
@@ -1,28 +1,10 @@
 import { routesMock } from "../data/routesMock.js"
 import planets from "../data/planetsMock.js"
+import { findAllRouteOffers } from "../utils/filterUtils.js"
 
-const routes = routesMock.legs
-
-export function findRoutes(planet, destinationPlanet, currentRoute = null, recursivePlanetHistory = [], recursiveRouteHistory = []) {
-    const history = []
-    const routeHistory = currentRoute ? recursiveRouteHistory.concat(currentRoute) : []
-    const planetHistory = recursivePlanetHistory.concat(planet)
-    
-    const nextRoutes = routes.filter(route => route.routeInfo.from.name == planet)
-
-    for (const route of nextRoutes) {
-        const nextPlanet = route.routeInfo.to.name
-
-        if (destinationPlanet == nextPlanet) {
-            routeHistory.push(route)
-            history.push(routeHistory)
-            continue
-        }
-        if (planetHistory.includes(nextPlanet)) continue
-        history.push(...findRoutes(nextPlanet, destinationPlanet, route, planetHistory, routeHistory))
-    }
-
-    return history;
+export function findRoutes(from, to) {
+    const routes = routesMock.legs
+    return findAllRouteOffers(from, to, routes)
 }
 
 export function findPlanets() {
