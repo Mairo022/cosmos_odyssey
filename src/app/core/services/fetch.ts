e3039a09a15f347a95d1022ad1175abc8e34f414
Mairo022 <mairo022@outlook.com>
2024-07-07 15:27:14 +0300

    Add fetch option to load later

--- src/app/core/services/fetch.ts
+++ src/app/core/services/fetch.ts
@@ -1,11 +1,28 @@
-import { Observable, catchError, finalize, tap, throwError } from "rxjs"
+import { EMPTY, Observable, catchError, finalize, tap, throwError } from "rxjs"
 
 export class Fetch<TData> {
   private _isLoading = false
   private _hasError = false
-  private _action$: Observable<TData>
+  private _action$: Observable<TData> = EMPTY
 
-  constructor(action$: Observable<TData>) {
+  constructor(action$: Observable<TData> | undefined = undefined) {
+    if (!action$) return
+    this._isLoading = true
+    this._action$ = action$.pipe(
+      tap(() => {
+        this._hasError = false
+      }),
+      catchError(() => {
+        this._hasError = true
+        return throwError(() => new Error("Fetch error"))
+      }),
+      finalize(() => {
+        this._isLoading = false
+      })
+    )
+  }
+
+  load(action$: Observable<TData>): void {
     this._isLoading = true
     this._action$ = action$.pipe(
       tap(() => {
