610957dcd4418a300ccec6120c214af5160362bc
Mairo022 <mairo022@outlook.com>
2024-07-12 15:34:57 +0300

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ export const app = express()
 
 app.use(express.json())
 
-app.use((req, res, next) => {
+app.use((_, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', "http://localhost:4200")
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
     next()
 })
 app.get("/api/routes", getRoutes)
