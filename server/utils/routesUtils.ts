0e053ee579796fe30c8220443aaa4ab5e3b27ccd
Mairo022 <mairo022@outlook.com>
2024-10-03 14:22:22 +0300

    Fix flights search

--- server/utils/routesUtils.ts
+++ server/utils/routesUtils.ts
@@ -8,12 +8,12 @@ export function findAllFlightOffers(routes: RoutesProviders, from: string, to: s
 }
 
 function findRoutes(
-  routes: RoutesProviders, 
-  from: string, 
-  to: string, 
-  beenTo = new Array<string>, 
+  routes: RoutesProviders,
+  from: string,
+  to: string,
+  beenTo = new Array<string>,
   exploredRoutes: RoutesProviders = []
-  ): Array<RoutesProviders> 
+  ): Array<RoutesProviders>
   {
   let fullRoute: Array<RoutesProviders> = []
   const nextRoutes = routes.filter(route => route.from === from && !beenTo.includes(route.to))
@@ -35,10 +35,16 @@ function getLinkedProviderOffers(fullRoutes: Array<RoutesProviders>): Array<any>
   const providers = new Array()
 
   for (const route of fullRoutes) {
-    const fullRouteCompanies = getFullRouteProvidingCompanies(route)
+    const fullRouteCompanies = findFullRouteProviders(route)
+
+    if (!Array.isArray(route[0].providers)) {
+      continue
+    }
 
     for (const provider of route[0].providers) {
-      if (!fullRouteCompanies.has(provider.company))
+      const isOutdated = new Date().getTime() + 1000 * 60 * 60 * 2 > new Date(provider.start).getTime()
+
+      if (!fullRouteCompanies.has(provider.company) || isOutdated)
           continue
 
       const from = route[0].from
@@ -51,7 +57,7 @@ function getLinkedProviderOffers(fullRoutes: Array<RoutesProviders>): Array<any>
           providers.push(linkedProviderOffer)
     }
   }
-  
+
   return providers
 }
 
@@ -75,6 +81,10 @@ function getLinkedProviderOffer(routes: RoutesProviders, providerFirst: Provider
   for (const route of routes) {
       if (isFirstRoute == 1) {
           isFirstRoute = 0
+          continue
+      }
+
+      if (!Array.isArray(route.providers)) {
           continue
       }
 
@@ -114,11 +124,16 @@ function getLinkedProviderOffer(routes: RoutesProviders, providerFirst: Provider
   return isOfferComplete ? history : []
 }
 
-function getFullRouteProvidingCompanies(route: RoutesProviders): Set<string> {
+// Finds route providers that offer full flights from Origin to Destination
+function findFullRouteProviders(route: RoutesProviders): Set<string> {
   let providers = new Set<string>
 
   for (let i = 0; i < route.length; i++) {
       const tempProviders = new Set<string>
+
+      if (!Array.isArray(route[i].providers)) {
+        continue
+      }
 
       for (let j = 0; j < route[i].providers.length; j++) {
           const provider = route[i].providers[j].company
